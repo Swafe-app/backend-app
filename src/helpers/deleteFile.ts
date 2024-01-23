a0d3b5a4a6eb6ca5177c27a251579f23393bce86
Fabien Fiette <fabienfiette@hotmail.fr>
2024-01-23 22:29:02 +0100

    Delete file helper

---
+++ src/helpers/deleteFile.ts
@@ -0,0 +1,13 @@
+import fs from 'fs';
+
+export const deleteFile = (filePath: string): Promise<void> => {
+  return new Promise((resolve, reject) => {
+    fs.unlink(filePath, (error) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve();
+      }
+    });
+  });
+};
