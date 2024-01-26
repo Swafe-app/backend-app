046939a25f6e82edd7bc8de2326f8bdf9d04808a
Fabien Fiette <fabienfiette@hotmail.fr>
2024-01-26 22:28:19 +0100

    Fix get image route

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,15 @@ import apiRouter from './routes/api';
 import sequelize from './services/sequelize';
 import connectWithRetry from './helpers/connectWithRetry';
 import { sendSuccess } from './helpers/response';
+import mime from 'mime-types';
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-// Configuration CORS, accept only 'swafe.app' domain and localhost
-const whitelist = [`http://localhost:3002`, `http://localhost:3000`, 'https://api.swafe.app', 'https://admin.swafe.app'];
+// Configuration CORS, accept all origins in development
+const whitelist = ['https://api.swafe.app', 'https://admin.swafe.app'];
 const corsOptions: CorsOptions = {
     origin: (origin: string | undefined, callback: any) => {
-        if (!origin || whitelist.indexOf(origin) !== -1) {
+        if (process.env.NODE_ENV === 'development' || !origin || whitelist.indexOf(origin) !== -1) {
             callback(null, true);
         } else {
             callback(`Origin ${origin} not allowed by CORS`, false);
@@ -29,6 +29,10 @@ app.use(express.json());
 app.get('/', (_req: Request, res: Response) => {
     sendSuccess(res, {
         routes: [
+            {
+                name: "Get image",
+                path: "/get-selfie/:filename",
+            },
             {
                 name: "Signalements",
                 path: "/signalements",
@@ -90,6 +94,10 @@ app.get('/', (_req: Request, res: Response) => {
                     {
                         method: "GET",
                         path: "/verifyEmail/:token"
+                    },
+                    {
+                        method: "POST",
+                        path: "/upload-selfie"
                     }
                 ]
             },
@@ -108,6 +116,10 @@ app.get('/', (_req: Request, res: Response) => {
                     {
                         method: "GET",
                         path: "/adminList"
+                    },
+                    {
+                        method: "PUT",
+                        path: "/updateUser"
                     }
                 ]
             }
@@ -119,7 +131,14 @@ app.get('/', (_req: Request, res: Response) => {
 app.use('/', apiRouter);
 
 // File static route
-app.use('/get-selfie', express.static('uploads/selfies'));
+app.use('/get-selfie', express.static('uploads/selfies', {
+    setHeaders: (res, filePath) => {
+        const type = mime.lookup(filePath);
+        if (type) {
+            res.setHeader('Content-Type', type);
+        }
+    }
+}));
 
 connectWithRetry(async () => {
     await sequelize.authenticate();
